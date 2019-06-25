7467f1717061c9f3fce024c4bbc1627c0558cd69
David Y. Stephenson <david@davidystephenson.com>
2019-06-25 15:58:38 +0200

    create

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4001
 const db = require('./db')
+const team = require('./team/model')
 app.listen(port, () => console.log(`Listening on :${port}`))
---
+++ team/model.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+const Team = db.define(
+  'team',
+  {
+    name: Sequelize.STRING
+  }
+)
+module.exports = Team
